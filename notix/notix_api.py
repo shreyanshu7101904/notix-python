cd2269a3f376c27ad05be4d9812665c349cd6ec9
Shreyanshu Shankar <shreyanshu.shankar@getmyuni.com>
2021-06-17 09:05:49 +0530

    notix api response parsers

--- notix/notix_api.py
+++ notix/notix_api.py
@@ -1,6 +1,12 @@
+import requests as request_module
+
 from .constants import URL_PATHS
 from .exceptions import UrlPathException
 
+NOTIX_URL = "http://notix.io/api"
+
+
+# Notix base Url
 
 def get_url(path_name: str) -> str:
     """
@@ -9,6 +15,61 @@ def get_url(path_name: str) -> str:
     :return: url
     """
     try:
-        return "http://notix.io/api" + URL_PATHS[path_name]
+        return NOTIX_URL + URL_PATHS[path_name]
     except KeyError:
         raise UrlPathException
+
+
+class BaseNotix:
+    """Base Notix Wrapper class"""
+
+
+class BaseResponseParser:
+    """Base Response parser class for all api calls"""
+
+
+class ResponseParser(BaseResponseParser):
+
+    def __init__(self, response):
+        self._response = response
+
+    def __str__(self):
+        return f"Status Code = {self._response.status_code}, message = {self._response.text}"
+
+    def parse(self):
+        return {
+            "status_code": self._response.status_code,
+            "message": self._response.text
+        }
+
+
+class Notix(BaseNotix):
+    """
+    Notix class
+    """
+
+    def __init__(self, app_id, token):
+        self._app_id = app_id
+        self._request = request_module
+        self._params = {
+            "app_id": self._app_id
+        }
+        self._header = {
+            "Authorization-Token": token
+        }
+
+    def send_notification(self, data) -> dict:
+        """
+        Notification sender method for notix class
+        :param data: dict as mentioned in notix docs https://docs.notix.co/api-send.html as request body
+        :return: dict
+        """
+        response = self._request.request(
+            method="POST",
+            url=get_url("send"),
+            params=self._params,
+            headers=self._header,
+            json=data
+        )
+        return ResponseParser(response).parse()
+
--- tests/test_notix.py
+++ tests/test_notix.py
@@ -1,4 +1,33 @@
-from notix.notix_api import get_url
+from unittest import TestCase
+from requests import Response
+from notix.notix_api import get_url, ResponseParser
+from notix.exceptions import UrlPathException
 
-def test_get_url():
-    assert get_url("send") == "http://notix.io/api/send"
+
+class GetUrlTest(TestCase):
+    """
+    `get_url` function test case class
+    """
+    def test_get_url(self):
+        """ Test output from `get_url` function"""
+        self.assertTrue("http://notix.io/api/send", get_url("send"))
+
+    def test_get_url_exception(self):
+        """ Test `UrlPathException` from `get_url` """
+        self.assertRaises(UrlPathException, get_url, "test")
+
+
+class ResponseParserTest(TestCase):
+    """Response parser base class to test notifications"""
+    response = Response()
+
+    def test_bad_response_code(self):
+        """test bad response code"""
+        self.response.status_code = 400
+        resp = ResponseParser(self.response).parse()
+        self.assertTrue("400",resp["status_code"])
+
+    def test_success_response_code(self):
+        self.response.status_code = 200
+        resp = ResponseParser(self.response).parse()
+        self.assertTrue("200", resp["status_code"])
