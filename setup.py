8a332cd599b8040e441b9ab3cf8ebc7a0f671072
shreyanshu7101904 <shreyanshu7101904@gmail.com>
2021-06-21 01:45:49 +0530

    version reordering

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="notix-python",
-    version="0.0.1",
+    version="1.0.0",
     description="Python Notix Push notification wrapper",
     author="Shreyanshu Shankar",
     long_description_content_type="text/markdown",
