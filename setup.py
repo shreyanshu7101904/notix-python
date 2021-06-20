7612a1537b19f6f84f8524da78389049396e1e2d
shreyanshu7101904 <shreyanshu7101904@gmail.com>
2021-06-21 01:50:50 +0530

    version reordering

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import find_packages, setup
+from os import path
+current_dir = path.abspath(path.dirname(__file__))
+with open(path.join(current_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="notix-python",
     version="1.0.0",
     description="Python Notix Push notification wrapper",
     author="Shreyanshu Shankar",
-    long_description_content_type="text/markdown",
+    long_description_content_type=current_dir,
     author_email="shreyanshu7101904@gmail.com",
     url="https://github.com/shreyanshu7101904/notix-push-notification",
     packages=find_packages(include=["notix"]),
